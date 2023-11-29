ebf2e317659259e6838fd8baea3c45610434b548
Alena Katkova <alena.katkova1996@gmail.com>
2023-11-29 23:46:36 +0300

    change test

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+    }
+}
---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.withArgs;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void newTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .log().all()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+                .prettyPeek()
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
